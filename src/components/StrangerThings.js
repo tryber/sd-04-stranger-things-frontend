9ee46a165e85cac2292e8bebad71268151baac31
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-10 20:41:59 -0300

    Comment

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -6,7 +6,7 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
-// Configurações
+// Setup
 const strangerThingsConfig = {
   url: process.env.REACT_APP_HAWKINS_URL,
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
