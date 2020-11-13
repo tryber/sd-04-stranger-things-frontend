1d1883967a104e787c0293d21952ebfd62492409
orlando-messias <programadorlando@gmail.com>
2020-11-13 13:08:43 -0300

    finish

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -6,7 +6,7 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
-// config
+// setup
 const strangerThingsConfig = {
   url: process.env.REACT_APP_HAWKINS_URL,
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
