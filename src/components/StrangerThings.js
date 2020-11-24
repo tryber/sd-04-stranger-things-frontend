179fe38ac6e8e5fb1308f3fd7e5496e2feb7c0b0
henrique <hbaetaleite@gmail.com>
2020-11-24 19:40:05 -0300

    deploy ft

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -101,7 +101,6 @@ class StrangerThings extends React.Component {
   render() {
     return (
       <div className={`reality ${getRealityClass(this.state.hereIsTheUpsideDownWorld)}`}>
-        {process.env.REACT_APP_DEVELOPMENT === 'desenvolvimento' && <h1>Em desenvolvimento</h1>}
         <div className="content strangerfy">
           <div className="change-reality">
             <button onClick={this.changeRealityClick}> Mudar de Realidade</button>
