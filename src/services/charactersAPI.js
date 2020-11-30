8bc97865f19ad1b931208708b3a0b67a5d4637f6
Julio Cesar Tinin <juliocesartinin@pop-os.localdomain>
2020-11-30 18:09:10 -0300

    Criando deploy do front

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 class CharactersService {
-  constructor({ url = 'http://localhost:3000', timeout = 30000 }) {
+  constructor({ url = 'http://localhost:3001', timeout = 30000 }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
