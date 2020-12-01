91268a3673144a97b6b03a5041c7ceb530ba4582
Ronald Lima <ronaldazevedolima@gmail.com>
2020-12-01 18:35:38 -0300

    teste3 dev

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -109,7 +109,7 @@ class StrangerThings extends React.Component {
           this.state.hereIsTheUpsideDownWorld
         )}`}
       >
-        {dev ? <span>Em desenvolvimento</span> : null}
+        {dev && <p>Em desenvolvimento</p>}
         <div className="content strangerfy">
           <div className="change-reality">
             <button onClick={this.changeRealityClick}>
