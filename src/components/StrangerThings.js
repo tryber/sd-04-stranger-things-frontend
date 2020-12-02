ae21f5e25fb35af1a92096c6e588d6cd46b6f630
djonata-dias <diasdjonata@gmail.com>
2020-12-02 10:20:40 -0300

    validação da variavel de ambiente de desenvolvimento

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -12,7 +12,7 @@ const strangerThingsConfig = {
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
-const developmentMode = process.env.REACT_APP_DEVELOPMENT_MODE;
+const developmentMode = process.env.REACT_APP_DEVELOPMENT_MODE === 'true';
 
 const upsideDownConfig = {
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
