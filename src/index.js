7facd2a348dd62f8e5dcbc76f4f7155667568b63
lourenco <loulage@gmail.com>
2020-12-02 14:47:25 -0300

    exchange pd and ft

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// mais comment
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
