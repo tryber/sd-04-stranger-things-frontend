5068f5d58be356cfc653b6a54d6702f4ed055ebb
RobertoAsgon <robertoasgon@gmail.com>
2020-12-03 07:43:29 -0400

    config frontend is done

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
+require('dotenv').config();
+
+const isDevMode = process.env.REACT_APP_DEVELOPMENT;
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
@@ -106,6 +109,7 @@ class StrangerThings extends React.Component {
       >
         <div className="content strangerfy">
           <div className="change-reality">
+            {(isDevMode === 'true') && <h1>Em desenvolvimento</h1>}
             <button onClick={this.changeRealityClick}>
               {' '}
               Mudar de Realidade
