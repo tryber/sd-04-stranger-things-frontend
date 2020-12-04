19bc7b4ddb477e3a5400aa791b97e32f33123306
felipedoss <felipecaetano25.1996@gmail.com>
2020-12-04 18:40:10 -0300

    teste

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -17,7 +17,7 @@ const upsideDownConfig = {
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
   env: process.env.REACT_APP_UPSIDEDOWN_URL,
 };
-
+// vhh
 const charactersService = new CharactersService(strangerThingsConfig);
 const charactersUpsideDownService = new CharactersService(upsideDownConfig);
 
