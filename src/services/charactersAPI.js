0e2407c0275ce6103be44f475063bd24aa8fbe27
Kaecio <kaecio@pop-os.localdomain>
2020-12-05 23:53:38 -0300

    Deploy front

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 class CharactersService {
-  constructor({ url = 'http://localhost:3000', timeout = 30000 }) {
+  constructor({ url = process.env.REACT_APP_HAWKINS_URL || 'http://localhost:3000', timeout = 30000 }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
