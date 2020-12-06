b0081ddbd02b42137334cbb3b253fe9026a71a4b
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-12-06 01:26:26 -0300

    usando variaveis de ambiente

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
@@ -107,7 +109,6 @@ class StrangerThings extends React.Component {
         <div className="content strangerfy">
           <div className="change-reality">
             <button onClick={this.changeRealityClick}>
-              {' '}
               Mudar de Realidade
             </button>
           </div>
