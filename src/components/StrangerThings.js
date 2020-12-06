44902818619d37f263a17c488b0f6ee4bb051ce6
Kaecio <kaecio@pop-os.localdomain>
2020-12-06 00:58:15 -0300

    Deploy

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -112,7 +112,7 @@ class StrangerThings extends React.Component {
               Mudar de Realidade
             </button>
           </div>
-          <p>Em desenvolvimento</p>
+
           <div>
             <input
               placeholder="Nome do Personagem"
